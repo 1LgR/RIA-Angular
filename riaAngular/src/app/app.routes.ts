bd90874c5b89c54b2a0d86b73ab279f6d6d43332
Luiz Gustavo Oliveira Rocha <130007640+1LgR@users.noreply.github.com>
2025-03-07 11:23:47 +0000

    :heavy_plus_sign: feat: Adiciona rotas. #5

--- riaAngular/src/app/app.routes.ts
+++ riaAngular/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { TaskListComponent } from './task-list.component';
+import { TaskFormComponent } from './task-form.component';
+import { TaskEditComponent } from './task-edit.component';
+import { TaskDetailComponent } from './task-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/tasks', pathMatch: 'full' },
+  { path: 'tasks', component: TaskListComponent },
+  { path: 'add', component: TaskFormComponent },
+  { path: 'edit/:id', component: TaskEditComponent },
+  { path: 'detail/:id', component: TaskDetailComponent },
+];
